ea7f386eb17f285773ba43d09613200e777c518a
evgeneh <mcfoody@ya.ru>
2020-02-10 00:11:33 +0300

    change port to PORT

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 //'mongodb://localhost/ontach-db', //MONGO_URL=
 
 module.exports = {
-    PORT: process.env.port || 4000,
+    PORT: process.env.PORT || 5000,
     MONGO_URL: process.env.MONGO_URL,
     SESSION_SECRET : process.env.SESSION_SECRET,
     FILE_STORAGE: process.env.FILE_STORAGE,
